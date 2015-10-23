4b8e547e643c44afa151aefa537ebe4964ca2e0a
Patrick O'Doherty <p@trickod.com>
2015-10-22 21:02:00 -0700

    Compiling but broken solution to 15

--- src/bin/1_5.rs
+++ src/bin/1_5.rs
@@ -1,9 +1,12 @@
 extern crate cryptopal;
+extern crate rustc_serialize;
 
 use std::fs::File;
 use std::io::BufReader;
 use std::io::Result;
 use std::io::prelude::*;
+
+use rustc_serialize::hex::ToHex;
 
 use cryptopal::xor;
 
@@ -16,18 +19,23 @@ fn read_data() -> Result<Vec<Vec<u8>>> {
 
     for line in reader.lines() {
         let line = try!(line);
-        lines.push(line);
+        lines.push(line.bytes().collect());
     }
 
-    return lines;
+    return Ok(lines);
 }
 
 fn main() {
-    let data = read_data();
-    let key = "ICE".to_vec();
+    let lines = match read_data() {
+        Ok(lines) => lines,
+        Err(e) => { panic!("Unable to load 5.txt: {:?}", e); }
+    };
+    let key = "ICE".as_bytes();
 
-    for line in data {
+    for line in lines {
         let xored = xor::repeating_xor(&line, &key);
-        println!("Xored: {}", String::from_utf8(xored).unwrap());
+        let hex = xored.to_hex();
+        println!("{}", String::from_utf8(line).unwrap());
+        println!("{}", hex);
     }
 }
