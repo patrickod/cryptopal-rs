f20f7ca681a4c360e8ef370998608eaa7aaec4ed
Patrick O'Doherty <p@trickod.com>
2016-05-05 00:31:07 +0100

    fix the pks7 padding ; add test

--- src/pkcs.rs
+++ src/pkcs.rs
@@ -1,7 +1,16 @@
+use std::iter::*;
+
 pub fn pad(bytes: &[u8], length: u8 ) -> Vec<u8> {
     let needed_bytes = length - bytes.len() as u8;
-    let padding = vec![4u8].iter().cycle().take(needed_bytes);
-    let padded = bytes.clone().to_owned().extend_from_slice(&padding);
-    return padded;
+    let padding: Vec<u8> = repeat(4u8).take(needed_bytes as usize).collect();
+    let mut result = bytes.clone().to_owned();
+    result.extend_from_slice(&padding);
+    return result;
 }
 
+#[test]
+fn test_pad() {
+    let input = "TEST".as_bytes();
+    let output = pad(input, 8);
+    assert_eq!(output, [84, 69, 83, 84, 4, 4, 4, 4]);
+}
