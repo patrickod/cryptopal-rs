0988b2244b0b817e5d7cb3d847c203711fd32043
Patrick O'Doherty <p@trickod.com>
2017-04-01 16:44:09 -0700

    Output 2_1 in hex instead of Vec<u8>

--- src/bin/2_1.rs
+++ src/bin/2_1.rs
@@ -1,9 +1,11 @@
 extern crate cryptopal;
+extern crate rustc_serialize;
 
 use cryptopal::pkcs::pad;
+use rustc_serialize::hex::ToHex;
 
 fn main() {
     let input = "YELLOW SUBMARINE".as_bytes();
     let padded = pad(input, 20);
-    println!("{:?}", padded);
+    println!("{:?}", padded.to_hex());
 }
