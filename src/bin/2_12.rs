ea6251fa47c7f1132faf601d96b17f7f0b973ca3
Patrick O'Doherty <p@trickod.com>
2020-07-18 18:57:19 -0700

    Smaller 2_12 solution to print out block size & ECB detection status

--- src/bin/2_12.rs
+++ src/bin/2_12.rs
@@ -2,22 +2,14 @@ extern crate cryptopal;
 extern crate rustc_serialize;
 
 use cryptopal::oracle::{Oracle, UnknownSuffixEcbOracle};
-
-fn determine_oracle_block_size_by_length<T: Oracle>(oracle: &T) -> Option<usize> {
-    let initial_length = oracle.encrypt(&vec![]).len();
-    for n in 0..20 {
-        let length = oracle.encrypt(&vec![0; n]).len();
-        if length != initial_length {
-            return Some(length - initial_length);
-        }
-    }
-    None
-}
+use cryptopal::util::{determine_oracle_block_size_by_length,detect_duplicate_blocks};
 
 fn main() {
     let oracle = UnknownSuffixEcbOracle::new();
-    println!(
-        "Oracle Block Length: {}",
-        determine_oracle_block_size_by_length(&oracle).unwrap()
-    );
+    let block_size = determine_oracle_block_size_by_length(&oracle).unwrap();
+
+    let uses_ecb = detect_duplicate_blocks(&oracle.encrypt(&vec![0; block_size * 2 + 1]));
+
+    println!("Oracle Block Size: {}", block_size);
+    println!("Oracle Uses ECB: {}", uses_ecb);
 }
