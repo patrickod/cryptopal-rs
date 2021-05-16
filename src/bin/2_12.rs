b008c69bf92af29f03049868e335ab7d13300248
Patrick O'Doherty <p@trickod.com>
2021-05-16 11:42:09 -0700

    2_12 variable renaming for clarity
    some new names to help refresh my memory

--- src/bin/2_12.rs
+++ src/bin/2_12.rs
@@ -9,21 +9,21 @@ fn main() {
     let mut recovered: Vec<u8> = Vec::new();
 
     let input = vec![0; block_size - 1];
-    let reference_ciphertexts = (0..block_size)
+    let reference_block_end_ciphertexts = (0..block_size)
         .map(|x| oracle.encrypt(&input[x..]))
         .collect::<Vec<Vec<u8>>>();
 
     for n in 0..calculate_payload_length(&oracle).unwrap() {
         let block_index = n / block_size;
         let byte_index = n % block_size;
+        let block_window_range = (block_index * block_size)..((block_index + 1) * block_size);
 
         for u in 0u8..=255 {
             let t = [input.clone(), recovered.clone(), vec![u; 1]].concat();
             let ciphertext = oracle.encrypt(&t[byte_index..]);
-            let block_range = (block_index * block_size)..((block_index + 1) * block_size);
 
-            if reference_ciphertexts[byte_index][block_range.clone()]
-                == ciphertext[block_range.clone()]
+            if reference_block_end_ciphertexts[byte_index][block_window_range.clone()]
+                == ciphertext[block_window_range.clone()]
             {
                 recovered.push(u);
                 break;
