c182caa7a3f3b89e68829b073640ccce9923434d
Patrick O'Doherty <p@trickod.com>
2021-05-20 20:01:19 -0700

    Add util::pkcs w/ tests
    pkcs::pad first validates the padding before removing the cleaned value.

--- src/pkcs.rs
+++ src/pkcs.rs
@@ -8,17 +8,55 @@ pub fn pad(bytes: &[u8], block_size: usize) -> Vec<u8> {
     [bytes.to_owned(), padding].concat()
 }
 
-#[test]
-fn test_pad() {
-    assert_eq!(pad("TEST".as_bytes(), 8), [84, 69, 83, 84, 4, 4, 4, 4]);
+pub fn strip(bytes: &[u8]) -> Result<&[u8], &'static str> {
+    let padding_value = match bytes.last() {
+        Some(&b) => b,
+        None => return Err("empty"),
+    };
+    if padding_value as usize > bytes.len() {
+        return Err("invalid padding");
+    }
+    let content_length = bytes.len() - padding_value as usize;
+    match bytes[content_length..]
+        .iter()
+        .all(|&c| c == padding_value)
+    {
+        true => Ok(&bytes[..content_length]),
+        false => Err("invalid padding"),
+    }
+}
 
-    assert_eq!(pad("TESTS".as_bytes(), 8), [84, 69, 83, 84, 83, 3, 3, 3]);
+#[cfg(test)]
+mod tests {
+    use pkcs::*;
 
-    assert_eq!(
-        pad("ICE ICE BABY".as_bytes(), 12),
-        [
-            73, 67, 69, 32, 73, 67, 69, 32, 66, 65, 66, 89, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
-            12, 12
-        ]
-    )
+    #[test]
+    fn test_pad() {
+        assert_eq!(pad("TEST".as_bytes(), 8), [84, 69, 83, 84, 4, 4, 4, 4]);
+
+        assert_eq!(pad("TESTS".as_bytes(), 8), [84, 69, 83, 84, 83, 3, 3, 3]);
+
+        assert_eq!(
+            pad("ICE ICE BABY".as_bytes(), 12),
+            [
+                73, 67, 69, 32, 73, 67, 69, 32, 66, 65, 66, 89, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+                12, 12
+            ]
+        )
+    }
+
+    #[test]
+    fn test_strip() {
+        // round-trip through our own padding function
+        let plaintext = "ICE ICE BABY".as_bytes();
+        let correctly_padded = pad(plaintext, 8);
+        assert_eq!(Ok(plaintext), strip(&correctly_padded));
+
+        // incorrect padding value
+        let p2 = [
+                plaintext,
+                &repeat(5u8).take(4).collect::<Vec<u8>>()
+        ].concat();
+        assert_eq!(Err("invalid padding"), strip(&p2));
+    }
 }
