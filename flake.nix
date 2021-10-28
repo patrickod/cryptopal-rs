a60d4b6cf190e6cdcc95b4dcc8f0764b9b70a24b
Patrick O'Doherty <p@trickod.com>
2021-10-27 19:32:20 -0700

    migrate to nix flakes

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "rust ARM M-0 development";
+  inputs = {
+    nixpkgs.url      = "github:patrickod/nixpkgs/personal";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "clippy-preview"
+            "rust-src"
+            "rustfmt-preview"
+          ];
+          targets = [
+            "thumbv7em-none-eabihf"
+          ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            rust
+          ];
+        };
+      }
+    );
+}
